af231b8b38a1fe0e4ffde1f98fb22fd4f3e2f101
kogutc <kogutc@hotmail.com>
2015-07-07 09:54:44 -0400

    from main refresh every minute

--- JS/appFX.js
+++ JS/appFX.js
@@ -26,11 +26,12 @@ FXRateAPP.controller("rateControllerold", ['$scope', 'fxService', function ($sco
 FXRateAPP.controller("rateController", ['$scope','$resource','fxService', function ($scope, $resource,fxService) {
 
     $scope.rateType = fxService.rateType;
-    $scope.myUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCAD%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-    $scope.FXAPI  = $resource($scope.myUrl,{callback: "JSON_CALLBACK"},{get: {method: "JSONP"}});
-    $scope.fxResults= $scope.FXAPI.get({});
-    console.log($scope);
-    console.log($scope.fxResults);
+    $scope.myUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCAD%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys?';
+    $scope.FXAPI  = $resource($scope.myUrl,{callback: "JSON_CALLBACK"},{post: {method: "JSONP"}});
+    $scope.fxResults= $scope.FXAPI.post({});
+    console.log('showing fx:');
+    console.log( $scope.fxResults);
+
 }]);
 
 
@@ -39,12 +40,12 @@ FXRateAPP.controller("resultController", ['$scope','$resource','l2Service', func
     $scope.ticker = l2Service.ticker;
     $scope.myUrl = 'https://chartapi.finance.yahoo.com/instrument/3.0/' + $scope.ticker + '/chartdata;type=quote;ys=2015;yz=2/json';
     //console.log('targetting:' +  $scope.myUrl);
-    $scope.l2API  = $resource($scope.myUrl,{callback: "JSON_CALLBACK"},{get: {method: "JSONP"}});
-    $scope.l2results= $scope.l2API.get({});
+    $scope.l2API  = $resource($scope.myUrl,{callback: "JSON_CALLBACK"},{post: {method: "JSONP"}});
+    $scope.l2results= $scope.l2API.post({});
+    console.log('showing results:');
     console.log($scope.l2results);
-    $scope.parseDate= pDate;
-    console.log($scope);
-}]);
+    //$scope.parseDate= pDate;
+    }]);
 
 
 var pDate =function(dt){
